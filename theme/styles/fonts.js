58edc48bdf89648e80519e51fe413768e0492e0a
Austin <austinatk22@gmail.com>
2023-01-03 20:17:40 -0500

    Update body and link fonts

--- theme/styles/fonts.js
+++ theme/styles/fonts.js
@@ -1,7 +1,7 @@
 const fonts = {
     heading: `'IBM Plex Sans', sans-serif`,
-    body: `'Merriweather', sans-serif`,
-    link: `'Merriweather', sans-serif`
+    body: `'IBM Plex Sans', sans-serif`,
+    link: `'IBM Plex Sans', sans-serif`
 };
 
 export default fonts;
