0ca03c8d14fc1f21219722caa714696b25640960
Austin <austinatk22@gmail.com>
2023-05-07 15:15:34 -0400

    Add Craft CMS logo icon and brand colour

--- data/design-icons.js
+++ data/design-icons.js
@@ -1,40 +1,47 @@
 import {
-    SiFigma,
-    SiMiro,
-    SiAdobecreativecloud,
-    SiWordpress,
-    SiShopify,
-} from "react-icons/si";
+	SiFigma,
+	SiMiro,
+	SiAdobecreativecloud,
+	SiWordpress,
+	SiShopify,
+	SiCraftcms,
+} from 'react-icons/si';
 
 export const designIconSet = [
-    {
-        id: 0,
-        image: SiFigma,
-        color: "black",
-        title: "Figma",
-    },
-    {
-        id: 1,
-        image: SiMiro,
-        color: "brand.miroYellow",
-        title: "Miro",
-    },
-    {
-        id: 2,
-        image: SiAdobecreativecloud,
-        color: "brand.adobeRed",
-        title: "Adobe Creative Cloud",
-    },
-    {
-        id: 3,
-        image: SiWordpress,
-        color: "brand.wordpressBlue",
-        title: "WordPress",
-    },
-    {
-        id: 4,
-        image: SiShopify,
-        color: "brand.shopifyGreen",
-        title: "Shopify",
-    },
+	{
+		id: 0,
+		image: SiFigma,
+		color: 'black',
+		title: 'Figma',
+	},
+	{
+		id: 1,
+		image: SiMiro,
+		color: 'brand.miroYellow',
+		title: 'Miro',
+	},
+	{
+		id: 2,
+		image: SiAdobecreativecloud,
+		color: 'brand.adobeRed',
+		title: 'Adobe Creative Cloud',
+	},
+	{
+		id: 3,
+		image: SiWordpress,
+		color: 'brand.wordpressBlue',
+		title: 'WordPress',
+	},
+	{
+		id: 4,
+		image: SiShopify,
+		color: 'brand.shopifyGreen',
+		title: 'Shopify',
+	},
+	{
+		id: 5,
+		image: SiCraftcms,
+		color: 'brand.craftRed',
+		title: 'Craft CMS',
+	},
 ];
